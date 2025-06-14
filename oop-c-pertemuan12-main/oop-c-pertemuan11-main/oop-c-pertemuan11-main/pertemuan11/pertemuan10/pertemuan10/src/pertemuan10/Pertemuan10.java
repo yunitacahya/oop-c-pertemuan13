c5f882a2b32268499469f2c8cf19edad42c95011
yunitacahya <yunitacahya998@gmail.com>
2025-06-14 14:50:39 +0800

    Add files via upload

---
+++ oop-c-pertemuan12-main/oop-c-pertemuan11-main/oop-c-pertemuan11-main/pertemuan11/pertemuan10/pertemuan10/src/pertemuan10/Pertemuan10.java
@@ -0,0 +1,17 @@
+package pertemuan10;
+
+/**
+ *
+ * @author Yunita Cahya
+ * TGL 24 MEI 2025
+ */
+public class Pertemuan10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
